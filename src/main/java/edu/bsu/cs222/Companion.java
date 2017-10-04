45784f8298fd1f9443dc6f6ef95a05576d36c130
Josh Mooshian <moosh223@gmail.com>
2017-10-04 13:46:26 -0400

    Add preliminary fxml file.

--- src/main/java/edu/bsu/cs222/Companion.java
+++ src/main/java/edu/bsu/cs222/Companion.java
@@ -1,7 +1,14 @@
 package edu.bsu.cs222;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TabPane;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class Companion extends Application {
 
@@ -10,7 +17,9 @@ public class Companion extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage) throws IOException{
+        Parent parent = FXMLLoader.load(getClass().getClassLoader().getResource("exampleDND.fxml"));
+        primaryStage.setScene(new Scene(parent));
         primaryStage.show();
     }
 }
