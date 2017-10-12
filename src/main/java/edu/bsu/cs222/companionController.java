b7012c2d784d58f2e5a165fc756b17835309af26
rcrandall84 <r20012001@yahoo.com>
2017-10-11 22:31:15 -0400

    added additional scenes

--- src/main/java/edu/bsu/cs222/companionController.java
+++ src/main/java/edu/bsu/cs222/companionController.java
@@ -2,8 +2,14 @@ package edu.bsu.cs222;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
+
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
 
 
 public class companionController {
@@ -14,9 +20,15 @@ public class companionController {
     @FXML private AnchorPane loadPane;
     @FXML private AnchorPane namePane;
     @FXML private AnchorPane classPane;
-    @FXML private Label errorLabel;
+    @FXML private AnchorPane statsPane;
+    @FXML private Label nameErrorLabel;
+    @FXML private Label classErrorLabel;
+    @FXML private Label levelErrorLabel;
     @FXML private TextField playerNameTextBox;
     @FXML private TextField characterNameTextBox;
+    @FXML private TextField raceTextBox;
+    @FXML private TextField classTextBox;
+    @FXML private TextField lvlTextBox;
 
     @FXML
     public void playerButtonPress(){
@@ -50,13 +62,47 @@ public class companionController {
     @FXML
     public void nameNextButtonPress(){
         if (playerNameTextBox.getText().equals("") || characterNameTextBox.getText().equals("")){
-            errorLabel.setVisible(true);
+            nameErrorLabel.setVisible(true);
         }
         else {
             String playerName = playerNameTextBox.getText();
-            String CharacterName = characterNameTextBox.getText();
+            String characterName = characterNameTextBox.getText();
             namePane.setVisible(false);
             classPane.setVisible(true);
+        }
+    }
+
+    @FXML
+    public void classNextButtonPress(){
+        try {
+            if (raceTextBox.getText().equals("") || classTextBox.getText().equals("") || lvlTextBox.getText().equals("")) {
+                classErrorLabel.setVisible(true);
+                levelErrorLabel.setVisible(false);
+            } else if (1 > Integer.parseInt(lvlTextBox.getText()) || 20 < Integer.parseInt(lvlTextBox.getText())) {
+                levelErrorLabel.setVisible(true);
+                classErrorLabel.setVisible(false);
+            } else {
+                String race = raceTextBox.getText();
+                String charClass = classTextBox.getText();
+                int level = Integer.parseInt(lvlTextBox.getText());
+                classPane.setVisible(false);
+                statsPane.setVisible(true);
+            }
+        }
+        catch(NumberFormatException e){
+            levelErrorLabel.setVisible(true);
+            classErrorLabel.setVisible(false);
+        }
+    }
+
+    @FXML
+    public void openPlayersHandbook(){
+        if (Desktop.isDesktopSupported()) {
+            try {
+                File file = new File("Players_Handbook.pdf");
+                Desktop.getDesktop().open(file);
+            } catch (IOException e) {
+            }
         }
     }
 }
