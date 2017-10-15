ceb6dcecc50416270b796e7e0731ed87896cf729
Josh Mooshian <moosh223@gmail.com>
2017-10-15 12:15:46 -0400

    Change XML tags to enumeration. Modify buildXML()

--- src/test/java/edu/bsu/cs222/CharacterTest.java
+++ src/test/java/edu/bsu/cs222/CharacterTest.java
@@ -4,14 +4,13 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class CharacterTest{
-    private int base_HP = 20;
-    private int base_STR, base_DEX, base_INT, base_CON, base_CHA, base_WIS = 10;
     private PlayerCharacter char1 = new PlayerCharacter("Example","Character Name");
 
     @Test
     public void testCharHP() {
         char1.setHP("25");
         int Health = Integer.parseInt(char1.getHP());
+        int base_HP = 20;
         Assert.assertNotEquals(Health, base_HP);
         Assert.assertEquals(Health, Integer.parseInt(char1.getHP()));
     }
@@ -19,6 +18,7 @@ public class CharacterTest{
     public void testCharSTR() {
         char1.setStat(0, 12);
         int Strength = char1.getStats()[0];
+        int base_STR = 23;
         assert(Strength != base_STR);
         assert(Strength == char1.getStats()[0]);
     }
@@ -26,6 +26,7 @@ public class CharacterTest{
     public void testCharDEX() {
         char1.setStat(1, 14);
         int Dexterity = char1.getStats()[1];
+        int base_DEX = 15;
         assert(Dexterity != base_DEX);
         assert(Dexterity == char1.getStats()[1]);
     }
@@ -33,6 +34,7 @@ public class CharacterTest{
     public void testCharCON() {
         char1.setStat(2, 8);
         int Constitution = char1.getStats()[2];
+        int base_CON = 14;
         assert(Constitution != base_CON);
         assert(Constitution == char1.getStats()[2]);
     }
@@ -40,6 +42,7 @@ public class CharacterTest{
     public void testCharINT() {
         char1.setStat(3, 13);
         int Intelligence = char1.getStats()[3];
+        int base_INT = 14;
         assert(Intelligence != base_INT);
         assert(Intelligence == char1.getStats()[3]);
     }
@@ -47,6 +50,7 @@ public class CharacterTest{
     public void testCharWIS() {
         char1.setStat(4, 15);
         int Wisdom = char1.getStats()[4];
+        int base_WIS = 10;
         assert(Wisdom != base_WIS);
         assert(Wisdom == char1.getStats()[4]);
     }
@@ -54,12 +58,14 @@ public class CharacterTest{
     public void testCharCHA() {
         char1.setStat(5, 3);
         int Charisma = char1.getStats()[5];
+        int base_CHA = 12;
         assert(Charisma != base_CHA);
         assert(Charisma == char1.getStats()[5]);
     }
 
     @Test
     public void testRetrieveName(){
+        char1.setCharacterName("Character Name");
         Assert.assertEquals(char1.getCharacterName(),"Character Name");
     }
 }
